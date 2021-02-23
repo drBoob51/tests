654a4d45efeacb186b7a0551bc69cd1e9aa94689
DrBoob <sdanila551@gmail.com>
2021-02-23 13:17:54 +0300

    home work

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('Hello World!')
+let user = 'John Doe'
+console.log(user) // John Doe
+let student = 'Danila'
+console.log(student)
+user = student
+console.log(user) // Danila
+let test = 1
+test++ // 2
+test = test + 1
+console.log(test) // 3
+test = test - 1
+console.log(test) // 2
+test = true
+console.log(typeof test)
+console.log(test)
