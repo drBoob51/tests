7276ff9a6cf91b5a73a9195bdc9f66b06850356a
DrBoob <sdanila551@gmail.com>
2021-02-23 14:26:10 +0300

    home work

--- main.js
+++ main.js
@@ -6,10 +6,14 @@ user = student
 console.log(user) // Danila
 let test = 1
 test++ // 2
-test = test + 1
-console.log(test) // 3
-test = test - 1
-console.log(test) // 2
+test += '1'
+console.log(test) // 21
+test -= 1
+console.log(test) // 20
 test = true
-console.log(typeof test)
-console.log(test)
+console.log(typeof test) // boolean
+console.log(test) // true
+let numbers = [2,3,5,8]
+let prod = 1
+numbers.forEach(function(e){prod *= e;})
+console.log(prod)
