c9d6069befb1b45df1241424146e4b982bb43446
DrBoob <sdanila551@gmail.com>
2021-02-24 14:36:12 +0300

    home work

--- main.js
+++ main.js
@@ -19,7 +19,11 @@ numbers.forEach(function(e){prod *= e;})
 console.log(prod)
 let array = [2,5,8,15,0,6,20,3]
 for (let i = 0 ; i<array.length; i++) {
-   if (array[i] < 10 && array[i] > 5){
-    console.log(array[i])}
+   if (array[i] < 10 && array[i] > 5)
+    {console.log(array[i])}
+}
+for (let x = 0 ; x<array.length; x++) {
+    if (array[x]%2 === 0)
+    {console.log(array[x])}
 }
 
