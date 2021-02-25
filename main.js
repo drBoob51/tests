541cb55b02eea7943035dc8a82906211cb94b304
DrBoob <sdanila551@gmail.com>
2021-02-25 17:02:33 +0300

    home work

--- main.js
+++ main.js
@@ -13,6 +13,8 @@ console.log(test) // 20
 test = true
 console.log(typeof test) // boolean
 console.log(test) // true
+
+
 let numbers = [2,3,5,8]
 let prod = 1
 numbers.forEach(function(e){prod *= e;})
@@ -26,4 +28,39 @@ for (let x = 0 ; x<array.length; x++) {
     if (array[x]%2 === 0)
     {console.log(array[x])}
 }
+
+
+let array1 = ['a','b','c'];
+let array2 = [1,2,3];
+//let array3 = array1.concat(array2);
+//console.log(array3);
+let array3 = [...array1, ...array2];
+console.log(array3)
+
+let firstArray = ['a','b','c'];
+firstArray.push (1);
+firstArray.push (2);
+firstArray.push (3);
+console.log(firstArray)
+
+
+function isPalindrome(str) {
+    let len = str.length;
+    let result;
+
+    for (let i = 0; i < len; i++) {
+        if (str[i] === str[len - 1 - i]) {
+            result = 'yes';
+            return result;
+        }  else {
+            result = 'no';
+            return result;
+        }
+    }
+}
+test = isPalindrome('mom');
+test2 = isPalindrome('daddy');
+
+console.log(test);
+console.log(test2);
 
